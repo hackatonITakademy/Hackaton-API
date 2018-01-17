6c0edf2f310c932f569f33634fa40d3d460acba6
Dylan <d.trochain@it-akademy.fr>
2018-01-17 16:22:45 +0100

    Création de la base de données via les migrations laravel.

---
+++ database/migrations/2018_01_17_150614_create_donations_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDonationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('donations', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->integer('currency_id')->unsigned();
+            $table->integer('amount');
+            $table->timestamps();
+
+            $table->foreign('currency_id')->references('id')->on('currencies');
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('donations');
+    }
+}
